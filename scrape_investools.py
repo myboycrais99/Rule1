20504d99c997667f6de492b763da33d4d30b5ca2
Ryan <rap4yd@gmail.com>
2017-01-06 07:48:07 -0700

    Updated line formatting

--- scrape_investools.py
+++ scrape_investools.py
@@ -31,21 +31,27 @@ def get_stock_data(beautiful_soup_result, search_str, root, tick, data_str):
 def stock_info(prop_index=0):
     ticker_file = r"C:\Users\Ryan\PycharmProjects\Rule1\tickers\{}.csv"
     prop = np.asarray(map(lambda s: s.replace('"', ''),
-                          np.genfromtxt(ticker_file.format("nasdaq_companylist"), dtype="S32", skip_header=1,
-                                        delimiter='",', comments='##')[:, prop_index]))
+                          np.genfromtxt(ticker_file.format(
+                              "nasdaq_companylist"), dtype="S32", skip_header=1,
+                              delimiter='",', comments='##')[:, prop_index]))
 
     prop = np.append(prop, map(lambda s: s.replace('"', ''),
-                               np.genfromtxt(ticker_file.format("nyse_companylist"), dtype="S32", skip_header=1,
-                                             delimiter='",', comments='##')[:, prop_index]))
+                               np.genfromtxt(ticker_file.format(
+                                   "nyse_companylist"), dtype="S32",
+                                   skip_header=1, delimiter='",',
+                                   comments='##')[:, prop_index]))
 
     # prop = np.append(prop, map(lambda s: s.replace('"', ''),
-    #                            np.genfromtxt(ticker_file.format("amex_companylist"), dtype="S32", skip_header=1,
-    #                                          delimiter='",', comments='##')[:, prop_index]))
+    #                            np.genfromtxt(ticker_file.format(
+    #                                "amex_companylist"), dtype="S32",
+    #                                skip_header=1, delimiter='",',
+    #                                comments='##')[:, prop_index]))
 
     return prop
 
 
-site = r"https://online.investools.com/authentication/auth.iedu?logout=true&brandLogoImage=identifier_investortoolbox"
+site = (r"https://online.investools.com/authentication/auth.iedu?logout="
+        "true&brandLogoImage=identifier_investortoolbox")
 site_root = r"https://toolbox.investools.com/graphs/fundamentalAnalysis"
 file_root = r"C:\Users\Ryan\PycharmProjects\Rule1\stock_data\\"
 
@@ -62,28 +68,41 @@ br.submit()
 n = 1
 num_tick = 6332
 tickers = np.asarray(["AAPL", "GOOG", "GOOGL"])
-# tickers = stock_info(prop_index=0)[n * int(num_tick / 20): (n + 1) * int(num_tick / 20)]
+# tickers = stock_info(prop_index=0)[n * int(num_tick / 20): (n + 1) *
+#                                    int(num_tick / 20)]
 
 for i, ticker in enumerate(tickers):
     ticker = ticker.strip()
     print("{:04.1f}%, {}".format((i + 1) / tickers.shape[0] * 100, ticker))
 
-    site_income_statement = r"{}.iedu?report=IS&symbol={}".format(site_root, ticker)
-    site_balance_sheet = r"{}.iedu?report=BS&symbol={}".format(site_root, ticker)
-    site_cash_flow = r"{}.iedu?report=CF&symbol={}".format(site_root, ticker)
-    site_trend_analysis = r"{}/trendAnalysis.iedu?symbol={}".format(site_root, ticker)
+    site_income_statement = r"{}.iedu?report=IS&symbol={}".format(site_root,
+                                                                  ticker)
+    site_balance_sheet = r"{}.iedu?report=BS&symbol={}".format(site_root,
+                                                               ticker)
+    site_cash_flow = r"{}.iedu?report=CF&symbol={}".format(site_root,
+                                                           ticker)
+    site_trend_analysis = r"{}/trendAnalysis.iedu?symbol={}".format(site_root,
+                                                                    ticker)
 
-    income_statement = BeautifulSoup(br.open(site_income_statement).read(), "html.parser")
-    balance_sheet = BeautifulSoup(br.open(site_balance_sheet).read(), "html.parser")
-    cash_flow = BeautifulSoup(br.open(site_cash_flow).read(), "html.parser")
-    trend_analysis = BeautifulSoup(br.open(site_trend_analysis).read(), "html.parser")
+    income_statement = BeautifulSoup(br.open(site_income_statement).read(),
+                                     "html.parser")
+    balance_sheet = BeautifulSoup(br.open(site_balance_sheet).read(),
+                                  "html.parser")
+    cash_flow = BeautifulSoup(br.open(site_cash_flow).read(),
+                              "html.parser")
+    trend_analysis = BeautifulSoup(br.open(site_trend_analysis).read(),
+                                   "html.parser")
 
     search_string = "Values in Millions of $"
     data_type = "income_statement"
 
-    get_stock_data(income_statement.find_all('table'), search_string, file_root, ticker, data_str="income_statement")
-    get_stock_data(balance_sheet.find_all('table'), search_string, file_root, ticker, data_str="balance_sheet")
-    get_stock_data(cash_flow.find_all('table'), search_string, file_root, ticker, data_str="cash_flow")
-    get_stock_data(trend_analysis.find_all('table'), "Period Ending:", file_root, ticker, data_str="trend_analysis")
+    get_stock_data(income_statement.find_all('table'), search_string, file_root,
+                   ticker, data_str="income_statement")
+    get_stock_data(balance_sheet.find_all('table'), search_string, file_root,
+                   ticker, data_str="balance_sheet")
+    get_stock_data(cash_flow.find_all('table'), search_string, file_root,
+                   ticker, data_str="cash_flow")
+    get_stock_data(trend_analysis.find_all('table'), "Period Ending:",
+                   file_root, ticker, data_str="trend_analysis")
 
     time.sleep(0.1)
