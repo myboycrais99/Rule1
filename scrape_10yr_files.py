29fa270dfe4755d21b273569e3e4b78d1a4b699e
Ryan <rap4yd@gmail.com>
2017-12-19 12:17:29 -0700

    Updated to python3 code

--- scrape_10yr_files.py
+++ scrape_10yr_files.py
@@ -4,7 +4,7 @@ pull out the relevant financial data necessary to calculate the Big 5 from
 Rule #1.
 """
 
-from __future__ import division, print_function
+
 from datetime import datetime
 from bs4 import BeautifulSoup
 from scrape_investools import stock_info
@@ -15,7 +15,7 @@ try:
     from urllib.request import urlopen
 except ImportError:
     # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+    from urllib.request import urlopen
 
 
 def open_file(tick, data_str, root=""):
@@ -27,7 +27,7 @@ def open_file(tick, data_str, root=""):
         return BeautifulSoup(fil, "html.parser")
 
 
-def parse_stock_data(tick, data_str):
+def get_stock_data(tick, data_str):
     """This function opens the 10-year stock data specified by 'data_str' for
     the passed stock ticker. Once open, the function parses the HTML data and
     returns a dictionary with keys taken from the first column of the website
@@ -64,16 +64,16 @@ def parse_stock_data(tick, data_str):
 
         # Check to see if data is Date row and if so, convert to datetime.
         if key == "Date":
-            val = map(lambda x: datetime.strptime(x, "%Y %m-%d"), val)
+            val = [datetime.strptime(x, "%Y %m-%d") for x in val]
 
         # Otherwise, convert all '-'s to NaNs, remove all commas, and convert
         # the values to floats.
         else:
-            val[val == "-"] = np.nan
+            val[val == "-"] = 0
 
-            val = map(lambda x: x.replace(",", ""), val)
+            val = [x.replace(",", "") for x in val]
 
-            val = map(lambda x: x.replace("+", ""), val)
+            val = [x.replace("+", "") for x in val]
 
             for idx, tmp in enumerate(val):
                 if tmp[-1] == "%":
@@ -82,30 +82,74 @@ def parse_stock_data(tick, data_str):
                     val[idx] = float(val[idx])
 
         # Add dictionary key value pair
-        stock_data[key] = val
+        stock_data[key] = np.asarray(val)
 
     return stock_data
 
 
 def main():
     """The main function"""
-    # tickers = ["AAPL", "GOOG", "GOOGL"]
+    tickers = ["AAPL", "GOOG", "GOOGL"]
     # tickers = tickers[1]
 
-    offset_start = 0
-    tickers = stock_info(prop_index=0)[offset_start:3]
+    # offset_start = 0
+    # tickers = stock_info(prop_index=0)[offset_start:3]
+
+    stocks = dict()
+
     for ticker in tickers[0:1]:
         print(ticker)
-        balance_sheet = parse_stock_data(ticker, "balance_sheet")
-        cash_flow = parse_stock_data(ticker, "cash_flow")
-        income_statement = parse_stock_data(ticker, "income_statement")
-        trend_analysis = parse_stock_data(ticker, "trend_analysis")
 
-        print(balance_sheet["Cash"])
-        print(cash_flow["Cash Taxes Paid"])
-        print(income_statement["Diluted Normalized EPS"])
-        print(trend_analysis["EPS"])
+        stocks[ticker] = dict(
+            list(get_stock_data(ticker, "balance_sheet").items()) +
+            list(get_stock_data(ticker, "cash_flow").items()) +
+            list(get_stock_data(ticker, "income_statement").items()) +
+            list(get_stock_data(ticker, "trend_analysis").items())
+        )
+
+        print("\tDate:", [x.year for x in stocks[ticker]["Date"]])
+
+        roic = (
+            stocks[ticker]["Operating Income"] * (
+                1 - (stocks[ticker]["Income Tax - Total"] /
+                     (stocks[ticker]["Net Income Before Taxes"])))) / (
+            stocks[ticker]["Total Liabilities"] +
+            stocks[ticker]["Common Stock, Total"] +
+            stocks[ticker]["Long Term Debt"]
+        )
+        print("\n\tROIC:", roic)
+
+
+
+        print("\n\tSales:", stocks[ticker]["Revenue Total"])
+        print("\tSales Growth Rate:",
+              stocks[ticker]["Revenue Total"][1:] /
+              stocks[ticker]["Revenue Total"][:-1] - 1)
+
+        eps = ((stocks[ticker]["Net Income"] -
+               stocks[ticker]["Preferred Dividends"]) /
+               stocks[ticker]["Diluted Weighted Average Shares"])
+        print("\n\tEPS:", eps)
+        print("\tEPS Growth Rate:", eps[1:] / eps[:-1] - 1)
+
+        bvps = (stocks[ticker]["Total Equity"] /
+                stocks[ticker]["Total Common Shares Outstanding"])
+        print("\n\tEquity:", bvps)
+        print("\tEquity Growth Rate:", bvps[1:] / bvps[:-1] - 1)
+
+        print("\n\tCash:", stocks[ticker]["Free Cash Flow"])
+        print("\tCash Growth Rate:",
+              stocks[ticker]["Free Cash Flow"][1:] /
+              stocks[ticker]["Free Cash Flow"][:-1] - 1)
 
 
 if __name__ == "__main__":
     main()
+
+    # Shiller P/E Ratio
+    # http: // www.multpl.com / cpi / table
+    # cpi = np.array(
+    #     [211.08, 211.14, 216.69, 220.22, 226.67, 230.28, 233.92, 233.71,
+    #      236.92, 241.43], dtype=float)
+    # data = stocks["AAPL"]["EPS"] / cpi * cpi[-1]
+    # print(data, np.mean(data))
--- scrape_investools.py
+++ scrape_investools.py
@@ -5,53 +5,41 @@ investools is required. This script then uses cookies to log in and scrap the
 relevant data. The income statements, balance sheets, cash flow, and trend
 analysis pages are scraped and saved to the local machine for later analysis.
 """
-from __future__ import division, print_function
-import mechanize
+
+import mechanicalsoup
 from bs4 import BeautifulSoup
 import numpy as np
 import time
-
-try:
-    # For Python 3.0 and later
-    import http.cookiejar
-except ImportError:
-    # For Python 3.0 and later
-    import cookielib
+import http.cookiejar
 
 
-    def get_stock_data(beautiful_soup_result, search_str, root, tick, data_str):
-        for txt in beautiful_soup_result:
 
-            if search_str in str(txt):
-                data = str(txt)
-                filename = r"{}{}_{}.html".format(root, tick, data_str)
+def get_stock_data(beautiful_soup_result, search_str, root, tick, data_str):
+    for txt in beautiful_soup_result:
 
-                with open(filename, "w") as f:
-                    f.write(data)
+        if search_str in str(txt):
+            data = str(txt)
+            filename = r"{}{}_{}.html".format(root, tick, data_str)
 
-                break
+            with open(filename, "w") as f:
+                f.write(data)
 
-    def stock_info(prop_index=0):
-        ticker_file = r"tickers\{}.csv"
-        prop = np.asarray(map(lambda s: s.replace('"', ''),
-                              np.genfromtxt(ticker_file.format(
-                                  "nasdaq_companylist"), dtype="S32",
-                                  skip_header=1, delimiter='",',
-                                  comments='##')[:, prop_index]))
+            break
 
-        prop = np.append(prop, map(lambda s: s.replace('"', ''),
-                                   np.genfromtxt(ticker_file.format(
-                                       "nyse_companylist"), dtype="S32",
-                                       skip_header=1, delimiter='",',
-                                       comments='##')[:, prop_index]))
 
-        # prop = np.append(prop, map(lambda s: s.replace('"', ''),
-        #                            np.genfromtxt(ticker_file.format(
-        #                                "amex_companylist"), dtype="S32",
-        #                                skip_header=1, delimiter='",',
-        #                                comments='##')[:, prop_index]))
+def stock_info(prop_index=0):
+    ticker_file = r"tickers\{}.csv"
 
-        return prop
+    prop = np.genfromtxt(ticker_file.format(
+        "nasdaq_companylist"), dtype="S32",
+        skip_header=1, delimiter='",',
+        comments='##')[:, prop_index]
+
+    prop = [s.decode("utf-8") for s in prop]
+
+    prop = np.asarray([s.replace('"', '') for s in prop])
+
+    return prop
 
 
 def main():
@@ -61,15 +49,16 @@ def main():
     site_root = r"https://toolbox.investools.com/graphs/fundamentalAnalysis"
     file_root = r"stock_data\\"
 
-    cj = cookielib.CookieJar()
-    br = mechanize.Browser()
+    cj = http.cookiejar.CookieJar()
+    br = mechanicalsoup.StatefulBrowser()
     br.set_cookiejar(cj)
     br.open(site)
 
     br.select_form(nr=0)
-    br.form['userid'] = r"rap4yd@gmail.com"
-    br.form['password'] = r"WedQ!t^2%ZE79ws"
-    br.submit()
+    br['userid'] = r"rap4yd@gmail.com"
+    br['password'] = r"WedQ!t^2%ZE79ws"
+
+    br.submit_selected()
 
     offset_start = 0
     tickers = stock_info(prop_index=0)[offset_start:3]
@@ -88,13 +77,13 @@ def main():
         site_trend_analysis = r"{}/trendAnalysis.iedu?symbol={}".format(
             site_root, ticker)
 
-        income_statement = BeautifulSoup(br.open(site_income_statement).read(),
+        income_statement = BeautifulSoup(br.open(site_income_statement).content,
                                          "html.parser")
-        balance_sheet = BeautifulSoup(br.open(site_balance_sheet).read(),
+        balance_sheet = BeautifulSoup(br.open(site_balance_sheet).content,
                                       "html.parser")
-        cash_flow = BeautifulSoup(br.open(site_cash_flow).read(),
+        cash_flow = BeautifulSoup(br.open(site_cash_flow).content,
                                   "html.parser")
-        trend_analysis = BeautifulSoup(br.open(site_trend_analysis).read(),
+        trend_analysis = BeautifulSoup(br.open(site_trend_analysis).content,
                                        "html.parser")
 
         search_string = "Values in Millions of $"
@@ -109,6 +98,7 @@ def main():
                        file_root, ticker, data_str="trend_analysis")
 
         time.sleep(0.1)
+
 
 if __name__ == "__main__":
     main()
